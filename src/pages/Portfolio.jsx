54a9d602c7cf416290094c9b6cec92d56b6cff5c
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-12-17 12:00:12 -0700

    add background pictures, styled the resume page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,6 +5,9 @@ import "./Portfolio.css";
 
 // Images used inside of each project (Only one currently)
 import Background from "../assets/images/backgroundimg.png";
+import JobhubBackground from "../assets/images/jobhubimage.png";
+import FinanceFlowBackground from "../assets/images/financeflowimage.png";
+import ProjectPalBackground from "../assets/images/projectpalimage.png";
 
 export default function Portfolio() {
 
@@ -17,17 +20,17 @@ export default function Portfolio() {
                     <Project
                     title={"ProjectPal"}
                     link="https://github.com/PeruvPowWow/ProjectPAL"
-                    backgroundImg={Background}
+                    backgroundImg={ProjectPalBackground}
                     />
                     <Project
                     title={"Finance Flow"}
                     link="https://github.com/Runnerrupert/finance-flow"
-                    backgroundImg={Background}
+                    backgroundImg={FinanceFlowBackground}
                     />
                     <Project
                     title={"JobHub"}
                     link="https://github.com/Runnerrupert/JobHub"
-                    backgroundImg={Background}
+                    backgroundImg={JobhubBackground}
                     />
                 </div>
             </div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,31 +1,43 @@
 import resume from '../assets/resume.txt';
+import "./Resume.css";
 
 export default function Resume() {
     return (
         <>
-            <div>
-                <a href={resume} download>Click Here to Download My Resume</a>
-            </div>
-            <div>
-                <h2>Front-End Proficiencies</h2>
-                <ul>
-                    <li className="resumeItem">HTML</li>
-                    <li className="resumeItem">CSS</li>
-                    <li className="resumeItem">Javascript</li>
-                    <li className="resumeItem">Web Design</li>
-                    <li className="resumeItem">React</li>
-                </ul>
-                <h2>Back-End Proficiencies</h2>
-                <ul>
-                    <li className="resumeItem">SQL</li>
-                    <li className="resumeItem">Node.js</li>
-                    <li className="resumeItem">Express.js</li>
-                </ul>
-                <h2>Other Proficiencies</h2>
-                <ul>
-                    <li className="resumeItem">Typing Speed: 80 Words Per Minute</li>
-                </ul>
-            </div>
+            <div className="resume-page">
+                <a href={resume} download className="download-link">Click Here to Download My Resume</a>
+                <div className="resume-container">
+                    <div className="proficiency-container">
+                        <h2>Front-End Proficiencies</h2>
+                        <ul>
+                            <li className="resumeItem">HTML</li>
+                            <li className="resumeItem">CSS</li>
+                            <li className="resumeItem">Javascript</li>
+                            <li className="resumeItem">Web Design</li>
+                            <li className="resumeItem">Responsive Design</li>
+                            <li className="resumeItem">React Router</li>
+                        </ul>
+                    </div>
+                    <div className="proficiency-container">
+                        <h2>Back-End Proficiencies</h2>
+                        <ul>
+                            <li className="resumeItem">SQL</li>
+                            <li className="resumeItem">Node.js</li>
+                            <li className="resumeItem">Express.js</li>
+                            <li className="resumeItem">MongoDB</li>
+                            <li className="resumeItem">JWT Tokens</li>
+                            <li className="resumeItem">Bcrypt</li>
+                            <li className="resumeItem">GraphQl</li>
+                        </ul>
+                    </div>
+                    <div className="proficiency-container">
+                        <h2>Other Proficiencies</h2>
+                        <ul>
+                            <li className="resumeItem">Typing Speed: 80 Words Per Minute</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>  
         </>
         
     )
